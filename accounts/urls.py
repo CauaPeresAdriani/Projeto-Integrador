b399e545d942d200d478c6a880aa5af9796ebadc
Cauã Peres Adriani <cauaperesacademico@gmail.com>
2026-08-24 12:46:41 -0300

    feat: implementa sistema completo de autenticação com 2FA e blindagem de rotas closes #6 closes #7

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home_view, name='home'),
     path('login/', views.meu_login_view, name='login'),
     path('setup_2fa/', views.meu_setup_2fa_view, name='setup_2fa'),
     path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('verificar_2fa/', views.verificar_2fa_view, name='verificar_2fa'),
+    path('logout/', views.meu_logout_view, name='logout'),
+    path('home/', views.home_view, name='home')
 ]
